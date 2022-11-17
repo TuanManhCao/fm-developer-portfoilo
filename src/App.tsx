38c71eee1ccc682201cff1a2699e8b73e080ae6d
Tuan Cao <caomanhtuanbsb@gmail.com>
2022-11-17 11:04:42 +0700

    Create vite -> react, typescript and add tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+// import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-red-100 text-2xl text-red-300'>Hi</h1>
+  )
+}
+
+export default App
