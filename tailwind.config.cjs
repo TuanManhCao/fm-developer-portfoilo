a71b2ff20251391e475f1f7da4a9255674a31b77
Tuan Cao <caomanhtuanbsb@gmail.com>
2022-11-17 12:06:26 +0700

    Config font, color and add assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,32 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tc-green': '#4EE1A0',
+        'tc-black': '#151515',
+        'tc-dark-grey': '#242424',
+        'tc-grey': '#D9D9D9',
+      },
+      fontFamily: {
+        sans: [
+          'Space Grotesk', 'sans'
+        ]
+      },
+      fontSize: {
+        '8xl': ['5.5rem', {
+          lineHeight: '5.5rem',
+          letterSpacing: '-2.5px',
+        }],
+        '4xl': ['3rem', {
+          lineHeight: '3.5rem',
+          letterSpacing: '-1.5px',
+        }],
+      }
+    },
+
+
+
   },
   plugins: [],
 }
